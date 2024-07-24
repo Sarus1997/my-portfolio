81fbdaf61523fed7ee6ee139b53ca7b5d8878ee5
Saharat Suwannapapond <saharat.rus@gmail.com>
2024-07-24 22:25:30 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Link } from "@nextui-org/link";
-import { button as buttonStyles } from "@nextui-org/theme";
+//import { Link } from "@nextui-org/link";
+//import { button as buttonStyles } from "@nextui-org/theme";
 
 import Hero from "@/components/content/Hero";
 
@@ -7,13 +7,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <div>ddefdegf</div>
-      <Link className={buttonStyles()} href="/about">
-        <h1>t</h1>
-      </Link>
-      <Link className={buttonStyles()} href="/about">
-        ergrgrgrg
-      </Link>
     </>
   );
 }
--- components/content/Hero.tsx
+++ components/content/Hero.tsx
@@ -3,6 +3,9 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { FaFileAlt, FaGithub } from "react-icons/fa";
 
 import styles from "../../styles/css/Hero.module.css";
 
@@ -26,10 +29,7 @@ function Hero() {
     <div className={styles.hero}>
       <div className={styles.container}>
         <div className={styles.row}>
-          <div
-            className={styles.left}
-            data-aos="fade-right" // AOS animation
-          >
+          <div className={styles.left} data-aos="fade-right">
             <h1 className={styles.title}>
               <span className={styles["title-part1"]}>Welcome To My </span>
               <br />
@@ -41,17 +41,32 @@ function Hero() {
               also enjoy petting every cat I see.
             </p>
           </div>
-          <div
-            className={styles.right}
-            data-aos="fade-left" // AOS animation
-          >
-            <img
-              alt="About Me"
-              className={styles.img}
-              src="/img/about-me.svg"
-            />
-            <LineDivider color={"success"} />
-            <AnimatedText />
+          <div data-aos="fade-left">
+            <div className={styles.right}>
+              <img
+                alt="About Me"
+                className={styles.img}
+                src="/img/about-me.svg"
+              />
+              <LineDivider color={"success"} />
+              <AnimatedText />
+            </div>
+            <div className={styles.buttonContainer}>
+              <Link
+                className={`${buttonStyles({ variant: "bordered", color: "primary" })} ${styles.button} ${styles.resumeButton}`}
+                href="/about"
+              >
+                <FaFileAlt className={styles.icon} />
+                Resume
+              </Link>
+              <Link
+                className={`${buttonStyles({ variant: "bordered", color: "secondary" })} ${styles.button} ${styles.githubButton}`}
+                href="/projects"
+              >
+                <FaGithub className={styles.icon} />
+                Github
+              </Link>
+            </div>
           </div>
         </div>
       </div>
